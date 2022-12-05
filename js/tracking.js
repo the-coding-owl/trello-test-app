cbb000d1f179872c048095f6a24fdff011304de6
Kevin Ditscheid <kevin@the-coding-owl.de>
2022-12-05 20:30:33 +0100

    Implement clock logic

--- js/tracking.js
+++ js/tracking.js
@@ -1,4 +1,31 @@
-var t = TrelloPowerUp.iframe();
+const t = TrelloPowerUp.iframe();
+const timeElement = window.tracker.querySelector('input');
+const buttonElement = window.tracker.querySelector('button');
+let timer = null;
+let time = 0;
+const startClock = function () {
+    console.log('Clock Started');
+    if (timer !== null) {
+        timer = null;
+        timeElement.value = '00:00';
+        buttonElement.textContent = 'Start';
+    } else {
+        timer = setInterval(() => {
+            let minutes = parseInt(time / 60, 10);
+            let seconds = parseInt(time % 60, 10);
+            timeElement.value = parseString(minutes).padStart(2,'0') + parseString(seconds).padStart(2,'0');
+        }, 1000);
+        buttonElement.textContent = 'End';
+    }
+    
+};
+
+window.tracker.addEventListener('submit', function(e) {
+    e.preventDefault();
+    t.startClock();
+    return ;   
+});
+
 t.render(function () {
     t.sizeTo('#tracker').done();
 });
