6133ed55bfa831709cf152875b3c14f52116506c
Kevin Ditscheid <kevin@the-coding-owl.de>
2022-12-05 20:38:25 +0100

    Fix timer ticking

--- js/tracking.js
+++ js/tracking.js
@@ -4,16 +4,19 @@ const buttonElement = window.tracker.querySelector('button');
 let timer = null;
 let time = 0;
 const startClock = function () {
-    console.log('Clock Started');
     if (timer !== null) {
+        console.log('Clock Stopped');
+        clearInterval(timer);
         timer = null;
         timeElement.value = '00:00';
         buttonElement.textContent = 'Start';
     } else {
+        console.log('Clock Started');
         timer = setInterval(() => {
             let minutes = parseInt(time / 60, 10);
             let seconds = parseInt(time % 60, 10);
-            timeElement.value = `${minutes}`.padStart(2,'0') + `${seconds}`.padStart(2,'0');
+            timeElement.value = `${minutes}`.padStart(2,'0') + ':' + `${seconds}`.padStart(2,'0');
+            time += 1;
         }, 1000);
         buttonElement.textContent = 'End';
     }
